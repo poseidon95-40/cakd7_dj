dbab11ee60591b211c5ccfce48cc21c4d61ec0ab
poseidon <poseidonyoung@gmail.com>
2022-08-05 12:10:09 +0900

    front page

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+#from django.views.generic import ListView, DetailView
+#from .models import Post
 
 # Create your views here.
+
+# class Landing(ListView):
+#     model = Post
+#     ordering = '-pk'
+#     template_name =  'single_pages/landing.html'
+
+# class About_Me(DetailView):
+#     model = Post    
+
+
+def landing(request):
+    return render(request, 'single_pages/landing.html')
+    
+def about_me(request):
+    return render(request, 'single_pages/about_me.html')
